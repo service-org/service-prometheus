e9ec2ab62720cccbf0f89ed58e91368908455054
forcemain <forcemain@163.com>
2021-11-05 18:25:13 +0800

    :underage:

---
+++ service_green/core/dependencies/prometheus.py
@@ -0,0 +1,102 @@
+#! -*- coding: utf-8 -*-
+#
+# author: forcemain@163.com
+
+from __future__ import annotations
+
+import typing as t
+
+from logging import getLogger
+from timeit import default_timer
+from prometheus_client import Gauge
+from prometheus_client import Counter
+from prometheus_client import Histogram
+from service_core.core.context import WorkerContext
+from service_core.core.service.dependency import Dependency
+
+logger = getLogger(__name__)
+
+
+class Prometheus(Dependency):
+    """ Prometheus依赖类
+
+    doc: https://github.com/prometheus/client_python
+    """
+
+    name = 'Prometheus'
+
+    def __init__(self, alias: t.Text, **kwargs: t.Any) -> None:
+        """ 初始化实例
+
+        @param alias: 配置别名
+        @param kwargs: 其它配置
+        """
+        self.alias = alias
+        self.req_current_count = None
+        self.splits_thread_count = None
+        self.worker_thread_count = None
+        self.req_latency_seconds = None
+        self.request_latency_seconds_map = {}
+        # 让每次请求都调用生命周期函数来自动生成指标数据
+        kwargs.setdefault('skip_callme', False)
+        super(Prometheus, self).__init__(**kwargs)
+
+    def setup(self) -> None:
+        """ 声明周期 - 载入阶段 """
+        self.splits_thread_count = Gauge(
+            'splits_thread_count', 'splits thread count', ('host',)
+        )
+        self.worker_thread_count = Gauge(
+            'worker_thread_count', 'worker thread count', ('host',)
+        )
+        self.req_current_count = Counter(
+            'req_current_count', 'request current count', ('server', 'driver', 'endpoint', 'status')
+        )
+        self.req_latency_seconds = Histogram(
+            'req_latency_seconds', 'request latency seconds', ('server', 'driver', 'endpoint', 'status')
+        )
+
+    def worker_setups(self, context: WorkerContext) -> None:
+        """ 工作协程 - 载入回调
+
+        @param context: 上下文对象
+        @return: None
+        """
+        host = self.container.service.host
+        self.splits_thread_count.labels(host).set(len(self.container.splits_threads))
+        self.worker_thread_count.labels(host).set(len(self.container.worker_threads))
+        self.request_latency_seconds_map.setdefault(context.worker_request_id, default_timer())
+
+    def worker_result(self, context: WorkerContext, results: t.Any) -> None:
+        """ 工作协程 - 正常回调
+
+        @param context: 上下文对象
+        @param results: 执行结果
+        @return: None
+        """
+        self.update_request_metrics(context, status='succ')
+
+    def worker_errors(self, context: WorkerContext, excinfo: t.Any) -> None:
+        """ 工作协程 - 异常回调
+
+        @param context: 上下文对象
+        @param excinfo: 异常对象
+        @return: None
+        """
+        self.update_request_metrics(context, status='fail')
+
+    def update_request_metrics(self, context: WorkerContext, status: t.Text):
+        """ 更新请求相关的指标
+
+        :param context: 上下文对象
+        :param status: 执行结果
+        :return: None
+        """
+        server = self.container.service.host
+        driver = context.original_entrypoint.name
+        request_id = context.worker_request_id
+        endpoint = context.original_entrypoint.object_name
+        start_time = self.request_latency_seconds_map.pop(request_id)
+        seconds = default_timer() - start_time
+        self.req_latency_seconds.labels(server, driver, endpoint, status).set(seconds)
+        self.req_current_count.labels(server, driver, endpoint, status).inc()
---
+++ service_green/core/middlewares/prometheus.py
@@ -0,0 +1,29 @@
+#! -*- coding: utf-8 -*-
+#
+# author: forcemain@163.com
+
+from __future__ import annotations
+
+import typing as t
+
+from prometheus_client import make_wsgi_app
+from service_core.core.service.entrypoint import Entrypoint
+from service_webserver.core.middlewares.dispatcher import DispatcherMiddleware
+
+if t.TYPE_CHECKING:
+    # 由于其定义在存根文件所以需要在TYPE_CHECKING下
+    from werkzeug.wsgi import WSGIApplication
+
+
+class PrometheusMiddleware(DispatcherMiddleware):
+    """ 普罗米修斯中间件类 """
+
+    def __init__(self, *, wsgi_app: WSGIApplication, producer: Entrypoint, **kwargs: t.Any) -> None:
+        """ 初始化实例
+
+        @param wsgi_app: 应用程序
+        @param producer: 服务提供者
+        @param kwargs: 命名参数
+        """
+        mounts = {'/metrics', make_wsgi_app()}
+        DispatcherMiddleware.__init__(self, wsgi_app=wsgi_app, producer=producer, mounts=mounts)
